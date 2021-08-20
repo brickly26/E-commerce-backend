acf40e77e78ea49a1ba80c77cebd8f0924148e19
Burak Aksu <sabburak26@gmail.com>
2021-08-20 16:41:16 -0400

    created all models

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,14 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
